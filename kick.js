f87a9db34ee42baad29b76e01680eb7d45ecf113
tahaefebingol <81387484+tahaefebingol@users.noreply.github.com>
2021-04-14 11:33:31 +0300

    Add files via upload

---
+++ kick.js
@@ -0,0 +1,39 @@
+const { MessageEmbed } = require('discord.js')
+module.exports= {
+  kod: "kick",
+  async run (client, message, args) {
+    if (!message.guild) return message.reply("Burası sunucu değil ki")
+    if (!message.member.hasPermission('ADMINISTRATOR')) return message.reply('Bunu senin yapmana yetkin yok')
+    const args1 = message.content.split(' ').slice(2).join(' ')
+    if (args1.length < 1) return message.reply('Bir sebep gir hadi')
+      const user = message.mentions.users.first();
+      if (!user) return message.reply('E hadi birisi etiketle')
+      if (user) {
+        const member = message.guild.member(user);
+        if (!member) return message.reply("Birisini etiketlemedin alooo")
+        if (member) {
+          member
+            .kick({reason: args1})
+            .then(x => {
+              const embed = new MessageEmbed()
+              .setTitle('BAŞARILI')
+              .setColor('GREEN')
+              .setTimestamp()
+              .addField("Atılan kişi ve ID'si", `${user.tag}   ` + `ID:  ${user.id}`)
+              .addField("Atılan kişi ve ID'si", `${message.author}   ` + `ID:  ${message.author.id}`)
+              .addField('Sebebi', args1, true)
+              message.channel.send(embed)
+            })
+            .catch(err => {
+              const embed = new MessageEmbed()
+              .setTitle('BAŞARISIZ')
+              .setColor('RED')
+              .setTimestamp()
+              .addField('Sebebi', "Bunu Atamıyorum.")
+              message.channel.send(embed)
+
+            });
+        }
+    }
+  }
+}
